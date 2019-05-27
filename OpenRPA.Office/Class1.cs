6668696d02ac032391cef1e6382418d667be0056
Allan Zimmermann <snigerosten@chatportal.dk>
2019-05-27 11:00:03 +0200

    update email for ff manifest

---
+++ OpenRPA.Office/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRPA.Office
+{
+    public class Class1
+    {
+    }
+}

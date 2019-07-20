92ff85d964e5e56845c10e7a2a96bf2ca7e605c7
Allan Zimmermann <snigerosten@chatportal.dk>
2019-07-20 16:40:55 +0200

    Add links to OpenFlow and NodeRED

--- OpenRPA.Net/Config.cs
+++ OpenRPA.Net/Config.cs
@@ -6,7 +6,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OpenRPA.Interfaces
+namespace OpenRPA
 {
     public class Config : AppSettings<Config>
     {
--- OpenRPA.Net/global.cs
+++ OpenRPA.Net/global.cs
@@ -1,4 +1,5 @@
-﻿using OpenRPA.Net;
+﻿using Newtonsoft.Json;
+using OpenRPA.Net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,5 +19,44 @@ namespace OpenRPA
                 return true;
             }
         }
+        private static openflowconfig _openflowconfig = null;
+        public static openflowconfig openflowconfig
+        {
+            get
+            {
+                if(_openflowconfig == null)
+                {
+                    using (System.Net.WebClient wc = new System.Net.WebClient())
+                    {
+                        try
+                        {
+                            var baseurl = new Uri(Config.local.wsurl);
+                            if (baseurl.Scheme == "wss") baseurl = new Uri(Config.local.wsurl.Replace("wss:", "https:"));
+                            if (baseurl.Scheme == "ws") baseurl = new Uri(Config.local.wsurl.Replace("ws:", "http:"));
+
+                            var url = new Uri(baseurl, "/config");
+                            var json = wc.DownloadString(url);
+                            _openflowconfig = JsonConvert.DeserializeObject<openflowconfig>(json);
+                            _openflowconfig.baseurl = baseurl.ToString();
+                        }
+                        catch (Exception)
+                        {
+                        }
+                    }
+                }
+                return _openflowconfig;
+            }
+        }
+
+    }
+    public class openflowconfig
+    {
+        public string wshost { get; set; }
+        public string domain { get; set; }
+        public bool allow_user_registration { get; set; }
+        public bool allow_personal_nodered { get; set; }
+        public string @namespace { get; set; }
+        public string nodered_domain_schema { get; set; }
+        public string baseurl { get; set; }
     }
 }

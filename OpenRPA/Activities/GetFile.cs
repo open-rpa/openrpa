0b765a440d1064b1ce8d16e78e880eabf8b3660f
Allan Zimmermann <snigerosten@chatportal.dk>
2019-10-17 13:16:10 +0200

    Use HTTP instead of sockets for getting files

--- OpenRPA/Activities/GetFile.cs
+++ OpenRPA/Activities/GetFile.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using Newtonsoft.Json.Linq;
 
 namespace OpenRPA.Activities
 {
@@ -31,8 +32,31 @@ namespace OpenRPA.Activities
             var id = _id.Get(context);
             var filepath = LocalPath.Get(context);
             var ignorepath = IgnorePath.Get(context);
-            await global.webSocketClient.DownloadFileAndSave(filename, id, filepath, ignorepath);
-            return 13;
+            // await global.webSocketClient.DownloadFileAndSave(filename, id, filepath, ignorepath);
+
+            Uri baseUri = new Uri(global.openflowconfig.baseurl);
+            filepath = Environment.ExpandEnvironmentVariables(filepath);
+
+            var q = "{\"_id\": \"" + id + "\"}";
+            if(!string.IsNullOrEmpty(filename)) q = "{\"filename\":\"" + filename + "\"}";
+            var rows = await global.webSocketClient.Query<JObject>("files", q);
+            if (rows.Length == 0) throw new Exception("File not found");
+            if (rows.Length == 0) throw new Exception("File not found");
+            filename = rows[0]["filename"].ToString();
+            id = rows[0]["_id"].ToString();
+
+            Uri downloadUri = new Uri(baseUri, "/download/" + id);
+            var url = downloadUri.ToString();
+
+            // if(string.IsNullOrEmpty(filename)) filename = "temp."
+            // if (System.IO.File.Exists(filepath) && !overwrite) return 42;
+            using (var client = new System.Net.WebClient())
+            {
+                // client.Headers.Add("Authorization", "jwt " + global.webSocketClient);
+                client.Headers.Add(System.Net.HttpRequestHeader.Authorization, global.webSocketClient.jwt);
+                await client.DownloadFileTaskAsync(new Uri(url), System.IO.Path.Combine(filepath, filename));
+            }
+            return 42;
         }
     }
 }

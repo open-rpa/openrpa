c1b318e2ef33ef44843a1ab0b4eed5d39592f103
Allan Zimmermann <snigerosten@chatportal.dk>
2020-02-24 08:43:16 +0100

    Google has accepted v10 of addon so change compensate_for_old_addon default to false

--- OpenRPA.NM/PluginConfig.cs
+++ OpenRPA.NM/PluginConfig.cs
@@ -24,6 +24,6 @@ namespace OpenRPA.NM
         public static bool wait_for_tab_after_set_value { get { return globallocal.GetProperty(pluginname, false); } set { globallocal.SetProperty(pluginname, value); } }
         public static bool wait_for_tab_click { get { return globallocal.GetProperty(pluginname, false); } set { globallocal.SetProperty(pluginname, value); } }
         public static bool register_old_portname { get { return globallocal.GetProperty(pluginname, true); } set { globallocal.SetProperty(pluginname, value); } }
-        public static bool compensate_for_old_addon { get { return globallocal.GetProperty(pluginname, true); } set { globallocal.SetProperty(pluginname, value); } }
+        public static bool compensate_for_old_addon { get { return globallocal.GetProperty(pluginname, false); } set { globallocal.SetProperty(pluginname, value); } }
     }
 }

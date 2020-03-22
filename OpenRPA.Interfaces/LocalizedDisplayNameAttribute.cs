c39a4fdce0704a63abee539dbd3e22b4e3bd2435
Allan Zimmermann <snigerosten@chatportal.dk>
2020-03-22 16:53:56 +0100

    Add LocalizedDisplayNameAttribute

---
+++ OpenRPA.Interfaces/LocalizedDisplayNameAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRPA.Interfaces
+{
+    public class LocalizedDisplayNameAttribute : DisplayNameAttribute
+    {
+        private readonly PropertyInfo nameProperty;
+        public LocalizedDisplayNameAttribute(string displayNameKey, Type resourceType = null) : base(displayNameKey)
+        {
+            if (resourceType != null) nameProperty = resourceType.GetProperty(base.DisplayName, BindingFlags.Static | BindingFlags.Public);
+        }
+        public override string DisplayName
+        {
+            get
+            {
+                if (nameProperty == null) return base.DisplayName;
+                return (string)nameProperty.GetValue(nameProperty.DeclaringType, null);
+            }
+        }
+    }
+}

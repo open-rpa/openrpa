aa24c0d198cfec62ad873c50624dd7cf3a6b021f
Allan Zimmermann <snigerosten@chatportal.dk>
2020-04-25 12:09:51 +0200

    Add ng-model as prefered selector

--- OpenRPA.NativeMessagingHost/openrpautil.js
+++ OpenRPA.NativeMessagingHost/openrpautil.js
@@ -858,6 +858,9 @@ if (true == false) {
                     return null; // Error.
                 switch (node.nodeType) {
                     case Node.ELEMENT_NODE:
+                        // 
+                        if (optimized && node.getAttribute("ng-model"))
+                            return new UTILS.DOMNodePathStep("//*[@ng-model=\"" + node.getAttribute("ng-model") + "\"]", true);
                         if (optimized && node.getAttribute("ng-reflect-name"))
                             return new UTILS.DOMNodePathStep("//*[@ng-reflect-name=\"" + node.getAttribute("ng-reflect-name") + "\"]", true);
                         if (optimized && node.getAttribute("aria-label"))

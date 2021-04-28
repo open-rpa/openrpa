aeeb40e69c490a32539e82b9fd66d3f251332a8f
Allan Zimmermann <allan@openrpa.dk>
2021-04-28 15:52:08 +0200

    Temp fix for Get Emails

---
+++ OpenRPA.Office/PluginConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRPA.Office
+{
+    class PluginConfig
+    {
+        private static string pluginname => "Office";
+        private static Config _globallocal = null;
+        public static Config globallocal
+        {
+            get
+            {
+                if (_globallocal == null)
+                {
+                    _globallocal = Config.local;
+                }
+                return _globallocal;
+            }
+        }
+        public static int get_emails_max_folders { get { return globallocal.GetProperty(pluginname, 50); } set { globallocal.SetProperty(pluginname, value); } }
+    }
+}

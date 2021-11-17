d51afe7e6a765bbd6f2358180015f087c45722fb
Allan Zimmermann <allan@openrpa.dk>
2021-11-17 10:06:38 +0100

    Add autologin powershell cmdlets

---
+++ OpenRPA.PS/RemoveAutologin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenRPA.Interfaces;
+using System.Management.Automation;
+namespace OpenRPA.PS
+{
+    [Cmdlet(VerbsCommon.Remove, "AutoLogin")]
+    public class RemoveAutologin : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            NativeMethods.LsaPrivateData.RemoveAutologin();
+        }
+
+    }
+}
---
+++ OpenRPA.PS/SetAutoLogin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenRPA.Interfaces;
+using System.Management.Automation;
+namespace OpenRPA.PS
+{
+    [Cmdlet(VerbsCommon.Set, "AutoLogin")]
+    public class SetAutoLogin : Cmdlet
+    {
+        [Parameter(Mandatory = true)] public string Username { get; set; }
+        [Parameter(Mandatory = true)] public string Password { get; set; }
+        [Parameter()] public int LogonCount { get; set; }
+        protected override void ProcessRecord()
+        {
+            NativeMethods.LsaPrivateData.SetAutologin(Username, Password, LogonCount);
+        }
+
+    }
+}

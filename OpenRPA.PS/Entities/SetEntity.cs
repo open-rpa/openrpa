0219579b16cb81ab6bfd0f54deb1f814405698e8
Allan Zimmermann <allan@openrpa.dk>
2021-12-08 10:02:14 +0100

    Increased speed when adding multiple objects

--- OpenRPA.PS/Entities/SetEntity.cs
+++ OpenRPA.PS/Entities/SetEntity.cs
@@ -7,6 +7,7 @@ using OpenRPA.Interfaces;
 using System.Management.Automation;
 using Newtonsoft.Json.Linq;
 using System.Collections.ObjectModel;
+using Newtonsoft.Json;
 
 namespace OpenRPA.PS
 {
@@ -17,14 +18,17 @@ namespace OpenRPA.PS
     [Cmdlet(VerbsCommon.Set, "Entity")]
     public class SetEntity : OpenRPACmdlet, IDynamicParameters
     {
-        [Parameter(ValueFromPipeline = true, Position = 1, Mandatory = true, ParameterSetName = "withObject")]
-        public PSObject Object { get; set; }
+        [Parameter(ValueFromPipeline = true, Position = 1, Mandatory = true, ParameterSetName = "withObjects")]
+        public List<Object> Objects { get; set; }
         [Parameter(ValueFromPipeline = true, Position = 1, Mandatory = true, ParameterSetName = "withJson")]
         public string json { get; set; }
         [Parameter()] public string[] UniqueKeys { get; set; }
         [Parameter()] public string Type { get; set; }
         [Parameter()] public SwitchParameter SkipLowercaseName { get; set; }
+        [Parameter(ParameterSetName = "withObjects")] public SwitchParameter SkipResults { get; set; }
         private static RuntimeDefinedParameterDictionary _staticStorage;
+        [Parameter(Position = 2, ParameterSetName = "withObjects")]
+        public int BatchSize { get; set; }
         public object GetDynamicParameters()
         {
             if (_Collections == null)
@@ -55,89 +59,163 @@ namespace OpenRPA.PS
                 _staticStorage.TryGetValue("Collection", out CollectionRuntime);
                 string Collection = "";
                 if (CollectionRuntime.Value != null && !string.IsNullOrEmpty(CollectionRuntime.Value.ToString())) Collection = CollectionRuntime.Value.ToString();
-                JObject obj = null;
-                if (Object != null)
+                if (string.IsNullOrEmpty(Collection)) Collection = "entities";
+
+                // If not using UniqueKeys and we are trying to ADD multiple new items, use add many for better performance.
+                if (Objects != null && Objects.Count > 1 && MyInvocation.InvocationName == "Add-Entity" && (UniqueKeys == null || UniqueKeys.Length == 0))
                 {
-                    json = Object.toJson();
-                }
-                JObject tmpObject = JObject.Parse(json);
-                string col = Collection;
-                if (string.IsNullOrEmpty(col)) // If not overwriten by param, then check for old collection
-                {
-                    if (tmpObject.ContainsKey("__pscollection"))
+                    var colls = new Dictionary<string, List<JObject>>();
+                    var col = Collection;
+                    foreach (PSObject obj in Objects)
                     {
-                        col = tmpObject.Value<string>("__pscollection");
-                        tmpObject.Remove("__pscollection");
-                    }
-                    else { col = "entities"; }
-                }
-                else
-                {
-                    if (tmpObject.ContainsKey("__pscollection")) tmpObject.Remove("__pscollection");
-                }
-                if (!string.IsNullOrEmpty(Type))
-                {
-                    tmpObject["_type"] = Type;
-                }
-                if (!SkipLowercaseName.IsPresent)
-                {
-                    bool loopAgain = true;
-                    while (loopAgain)
-                    {
-                        loopAgain = false;
-                        foreach (var v in tmpObject)
+                        var json = obj.toJson();
+                        JObject tmpObject = JObject.Parse(json);
+                        col = Collection;
+                        if (tmpObject.ContainsKey("__pscollection") && tmpObject["__pscollection"] != null)
                         {
-                            if (v.Key.ToLower() == "name" && v.Key != "name")
+                            col = tmpObject.Value<string>("__pscollection");
+                            tmpObject.Remove("__pscollection");
+                        }
+                        if (!colls.ContainsKey(col)) colls.Add(col, new List<JObject>());
+                        colls[col].Add(tmpObject);
+                    }
+                    if (BatchSize <= 0) BatchSize = 15;
+                    if (BatchSize < 2) BatchSize = 2;
+                    foreach (var kv in colls)
+                    {
+                        var total = kv.Value.Count;
+                        for (var i = 0; i < total; i = i + BatchSize)
+                        {
+                            if (Stopping) break;
+                            var count = BatchSize;
+                            if ((i + count) > total) count = total - i;
+                            var items = kv.Value.GetRange(i, count).ToArray();
+                            var affectedrows = await global.webSocketClient.InsertMany(kv.Key, 1, false, SkipResults.IsPresent, items);
+                            WriteVerbose("Added " + (i + count) + " rows out of " + total + " to collection " + col);
+                            foreach (var obj in affectedrows)
                             {
-                                tmpObject["name"] = tmpObject[v.Key];
-                                tmpObject.Remove(v.Key);
-                                loopAgain = true;
-                                break;
+                                var _obj = obj.toPSObject();
+                                _obj.TypeNames.Insert(0, "OpenRPA.PS.Entity");
+                                if (Collection == "openrpa")
+                                {
+                                    if (obj.Value<string>("_type") == "workflow") _obj.TypeNames.Insert(0, "OpenRPA.Workflow");
+                                    if (obj.Value<string>("_type") == "project") _obj.TypeNames.Insert(0, "OpenRPA.Project");
+                                    if (obj.Value<string>("_type") == "detector") _obj.TypeNames.Insert(0, "OpenRPA.Detector");
+                                    if (obj.Value<string>("_type") == "unattendedclient") _obj.TypeNames.Insert(0, "OpenRPA.UnattendedClient");
+                                    if (obj.Value<string>("_type") == "unattendedserver") _obj.TypeNames.Insert(0, "OpenRPA.UnattendedServer");
+                                }
+                                WriteObject(_obj);
                             }
                         }
-
                     }
-                }
-                if (MyInvocation.InvocationName == "Add-Entity")
-                {
-                    if (tmpObject.ContainsKey("_id")) tmpObject.Remove("_id");
-                } 
-
-                if (UniqueKeys != null && UniqueKeys.Length > 0)
-                {
-                    string uniqeness = string.Join(",", UniqueKeys);
-                    obj = await global.webSocketClient.InsertOrUpdateOne(col, 1, false, uniqeness, tmpObject);
+                    return;
                 }
                 else
                 {
-                    if (tmpObject.ContainsKey("_id"))
+                    var PSObjects = new List<PSObject>();
+                    if (Objects == null || Objects.Count == 0)
                     {
-                        obj = await global.webSocketClient.UpdateOne(col, 1, false, tmpObject);
-                    } else
+                        PSObjects.Add(json.JsonToPSObject());
+                    }
+                    else
                     {
+                        foreach (var obj in Objects)
+                        {
+                            PSObjects.Add(JObject.FromObject(obj, new JsonSerializer()
+                            {
+                                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                            }).ToString().JsonToPSObject());
+                        }
+                    }
+                    foreach (PSObject Object in PSObjects)
+                    {
+                        JObject obj = null;
+                        if (Object != null)
+                        {
+                            json = Object.toJson();
+                        }
+                        JObject tmpObject = JObject.Parse(json);
+                        string col = Collection;
+                        if (string.IsNullOrEmpty(col)) // If not overwriten by param, then check for old collection
+                        {
+                            if (tmpObject.ContainsKey("__pscollection"))
+                            {
+                                col = tmpObject.Value<string>("__pscollection");
+                                tmpObject.Remove("__pscollection");
+                            }
+                            else { col = "entities"; }
+                        }
+                        else
+                        {
+                            if (tmpObject.ContainsKey("__pscollection")) tmpObject.Remove("__pscollection");
+                        }
+                        if (!string.IsNullOrEmpty(Type))
+                        {
+                            tmpObject["_type"] = Type;
+                        }
+                        if (!SkipLowercaseName.IsPresent)
+                        {
+                            bool loopAgain = true;
+                            while (loopAgain)
+                            {
+                                loopAgain = false;
+                                foreach (var v in tmpObject)
+                                {
+                                    if (v.Key.ToLower() == "name" && v.Key != "name")
+                                    {
+                                        tmpObject["name"] = tmpObject[v.Key];
+                                        tmpObject.Remove(v.Key);
+                                        loopAgain = true;
+                                        break;
+                                    }
+                                }
+
+                            }
+                        }
                         if (MyInvocation.InvocationName == "Add-Entity")
                         {
-                            obj = await global.webSocketClient.InsertOne(col, 1, false, tmpObject);
-                        } else
-                        {
-                            WriteError(new ErrorRecord(new Exception("Missing _id and UniqueKeys, either use Add-Entity or set UniqueKeys") , "", ErrorCategory.NotSpecified, null));
-                            return;
+                            if (tmpObject.ContainsKey("_id")) tmpObject.Remove("_id");
                         }
-                            
+
+                        if (UniqueKeys != null && UniqueKeys.Length > 0)
+                        {
+                            string uniqeness = string.Join(",", UniqueKeys);
+                            obj = await global.webSocketClient.InsertOrUpdateOne(col, 1, false, uniqeness, tmpObject);
+                        }
+                        else
+                        {
+                            if (tmpObject.ContainsKey("_id"))
+                            {
+                                obj = await global.webSocketClient.UpdateOne(col, 1, false, tmpObject);
+                            }
+                            else
+                            {
+                                if (MyInvocation.InvocationName == "Add-Entity")
+                                {
+                                    obj = await global.webSocketClient.InsertOne(col, 1, false, tmpObject);
+                                }
+                                else
+                                {
+                                    WriteError(new ErrorRecord(new Exception("Missing _id and UniqueKeys, either use Add-Entity or set UniqueKeys"), "", ErrorCategory.NotSpecified, null));
+                                    return;
+                                }
+
+                            }
+                        }
+                        var _obj = obj.toPSObject();
+                        _obj.TypeNames.Insert(0, "OpenRPA.PS.Entity");
+                        if (Collection == "openrpa")
+                        {
+                            if (obj.Value<string>("_type") == "workflow") _obj.TypeNames.Insert(0, "OpenRPA.Workflow");
+                            if (obj.Value<string>("_type") == "project") _obj.TypeNames.Insert(0, "OpenRPA.Project");
+                            if (obj.Value<string>("_type") == "detector") _obj.TypeNames.Insert(0, "OpenRPA.Detector");
+                            if (obj.Value<string>("_type") == "unattendedclient") _obj.TypeNames.Insert(0, "OpenRPA.UnattendedClient");
+                            if (obj.Value<string>("_type") == "unattendedserver") _obj.TypeNames.Insert(0, "OpenRPA.UnattendedServer");
+                        }
+                        WriteObject(_obj);
+                        await Task.Delay(1);
                     }
                 }
-                var _obj = obj.toPSObject();
-                _obj.TypeNames.Insert(0, "OpenRPA.PS.Entity");
-                if (Collection == "openrpa")
-                {
-                    if (obj.Value<string>("_type") == "workflow") _obj.TypeNames.Insert(0, "OpenRPA.Workflow");
-                    if (obj.Value<string>("_type") == "project") _obj.TypeNames.Insert(0, "OpenRPA.Project");
-                    if (obj.Value<string>("_type") == "detector") _obj.TypeNames.Insert(0, "OpenRPA.Detector");
-                    if (obj.Value<string>("_type") == "unattendedclient") _obj.TypeNames.Insert(0, "OpenRPA.UnattendedClient");
-                    if (obj.Value<string>("_type") == "unattendedserver") _obj.TypeNames.Insert(0, "OpenRPA.UnattendedServer");
-                }
-                WriteObject(_obj);
-                await Task.Delay(1);
             }
             catch (Exception ex)
             {

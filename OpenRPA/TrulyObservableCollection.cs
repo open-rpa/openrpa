3cb928e51aa435c889545b27a357f8f59dc204ad
Allan Zimmermann <allan@openrpa.dk>
2022-04-10 09:36:00 +0200

    add TrulyObservableCollection

---
+++ OpenRPA/TrulyObservableCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRPA
+{
+    public sealed class TrulyObservableCollection<T> : ObservableCollection<T>
+        where T : INotifyPropertyChanged
+    {
+        public TrulyObservableCollection()
+        {
+            CollectionChanged += FullObservableCollectionCollectionChanged;
+        }
+        public TrulyObservableCollection(IEnumerable<T> pItems) : this()
+        {
+            foreach (var item in pItems)
+            {
+                Add(item);
+            }
+        }
+        private void FullObservableCollectionCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null)
+            {
+                foreach (object item in e.NewItems)
+                {
+                    ((INotifyPropertyChanged)item).PropertyChanged += ItemPropertyChanged;
+                }
+            }
+            if (e.OldItems != null)
+            {
+                foreach (object item in e.OldItems)
+                {
+                    ((INotifyPropertyChanged)item).PropertyChanged -= ItemPropertyChanged;
+                }
+            }
+        }
+        private void ItemPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            NotifyCollectionChangedEventArgs args = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, sender, sender, IndexOf((T)sender));
+            OnCollectionChanged(args);
+        }
+    }
+}

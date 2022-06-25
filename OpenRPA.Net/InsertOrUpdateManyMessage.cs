a01e6640ec2cb61440419dc8633fa881af8869d1
Allan Zimmermann <allan@openrpa.dk>
2022-06-25 12:01:18 +0200

    add InsertOrUpdateMany to client

---
+++ OpenRPA.Net/InsertOrUpdateManyMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenRPA.Net
+{
+    class InsertOrUpdateMany<T> : SocketCommand
+    {
+        public InsertOrUpdateMany() : base()
+        {
+            msg.command = "insertorupdatemany";
+        }
+        // w: 1 - Requests acknowledgment that the write operation has propagated
+        // w: 0 - Requests no acknowledgment of the write operation
+        // w: 2 would require acknowledgment from the primary and one of the secondaries
+        // w: 3 would require acknowledgment from the primary and both secondaries
+        public int w { get; set; }
+        // true, requests acknowledgment that the mongod instances have written to the on-disk journal
+        public bool j { get; set; }
+        public T[] items { get; set; }
+        public string collectionname { get; set; }
+        public string uniqeness { get; set; }        
+        public T[] results { get; set; }
+        public bool skipresults { get; set; }
+    }
+}

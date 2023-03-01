cf32bf7665c86685b776043b7eb9cbb6eb4248bb
skadefro <allan@openrpa.dk>
2023-03-01 09:32:46 +0100

    remove nodered commands

--- OpenRPA.PS/NodeRED/AddNoderedInstance.cs
+++ OpenRPA.PS/NodeRED/AddNoderedInstance.cs
@@ -1,33 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using OpenRPA.Interfaces;
-using System.Management.Automation;
-using Newtonsoft.Json.Linq;
-using System.Collections.ObjectModel;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using OpenRPA.Interfaces;
+//using System.Management.Automation;
+//using Newtonsoft.Json.Linq;
+//using System.Collections.ObjectModel;
 
-namespace OpenRPA.PS
-{
-    [Cmdlet(VerbsCommon.Add, "NoderedInstance")]
-    public class AddNoderedInstance : OpenRPACmdlet
-    {
-        [Parameter(Position = 0, ValueFromPipeline = true)] public string _id { get; set; }
-        protected override async Task ProcessRecordAsync()
-        {
-            try
-            {
-                string traceId = ""; string spanId = "";
-                if (string.IsNullOrEmpty(_id)) { _id = global.webSocketClient.user._id; }
-                await global.webSocketClient.EnsureNoderedInstance(_id, traceId, spanId);
-            }
-            catch (Exception ex)
-            {
-                WriteError(new ErrorRecord(ex, "", ErrorCategory.NotSpecified, null));
-            }
+//namespace OpenRPA.PS
+//{
+//    [Cmdlet(VerbsCommon.Add, "NoderedInstance")]
+//    public class AddNoderedInstance : OpenRPACmdlet
+//    {
+//        [Parameter(Position = 0, ValueFromPipeline = true)] public string _id { get; set; }
+//        protected override async Task ProcessRecordAsync()
+//        {
+//            try
+//            {
+//                string traceId = ""; string spanId = "";
+//                if (string.IsNullOrEmpty(_id)) { _id = global.webSocketClient.user._id; }
+//                await global.webSocketClient.EnsureNoderedInstance(_id, traceId, spanId);
+//            }
+//            catch (Exception ex)
+//            {
+//                WriteError(new ErrorRecord(ex, "", ErrorCategory.NotSpecified, null));
+//            }
 
 
-        }
-    }
-}
+//        }
+//    }
+//}

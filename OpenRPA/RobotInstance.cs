982c415b628f4a331946b9ee38f2ec96930abec6
skadefro <allan@openrpa.dk>
2023-04-25 18:15:56 +0200

    Fix showing UI when using password from config

--- OpenRPA/RobotInstance.cs
+++ OpenRPA/RobotInstance.cs
@@ -1237,6 +1237,7 @@ namespace OpenRPA
                             Log.Debug("Signing in as " + Config.local.username + " " + string.Format("{0:mm\\:ss\\.fff}", sw.Elapsed));
                             user = await global.webSocketClient.Signin(Config.local.username, Config.local.UnprotectString(Config.local.password));
                             Log.Debug("Signed in as " + Config.local.username + " " + string.Format("{0:mm\\:ss\\.fff}", sw.Elapsed));
+                            Show();
                             SetStatus("Connected to " + Config.local.wsurl + " as " + user.name);
                             if(!string.IsNullOrEmpty(Config.local.unsafepassword))
                             {
